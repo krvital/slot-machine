1f3adba0f19efb22ca5f2726d958cb39ab45a05a
Vitalii Kravtsov <mail@krvital.pro>
2019-06-28 15:06:27 +0600

    add built version for github pages

---
+++ docs/precache-manifest.8a554bdab493767ffd6a57ac2bd6e29c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad242da88a604316401b9039e993542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49e55964c976b294ade",
+    "url": "/static/css/main.eed04245.chunk.css"
+  },
+  {
+    "revision": "eecc6995b722283c13d4",
+    "url": "/static/js/2.521f8165.chunk.js"
+  },
+  {
+    "revision": "f49e55964c976b294ade",
+    "url": "/static/js/main.7e51c7e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "565a57790e190fec06a5793e260d809f",
+    "url": "/static/media/cherry.565a5779.png"
+  }
+]);
