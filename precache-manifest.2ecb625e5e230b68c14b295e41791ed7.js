da5bc9af00f1eb30819e9f6efb2f9941cbce00a2
Vitalii Kravtsov <mail@krvital.pro>
2019-06-28 15:13:07 +0600

    Updates

---
+++ precache-manifest.2ecb625e5e230b68c14b295e41791ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17576d5c63d35171028b6fbaee7c07b",
+    "url": "/slot-machine/index.html"
+  },
+  {
+    "revision": "9a1b25c9a4fb6440495d",
+    "url": "/slot-machine/static/css/main.eed04245.chunk.css"
+  },
+  {
+    "revision": "eecc6995b722283c13d4",
+    "url": "/slot-machine/static/js/2.521f8165.chunk.js"
+  },
+  {
+    "revision": "9a1b25c9a4fb6440495d",
+    "url": "/slot-machine/static/js/main.4484d846.chunk.js"
+  },
+  {
+    "revision": "470de731605fcfe510a7",
+    "url": "/slot-machine/static/js/runtime~main.bb7216ed.js"
+  },
+  {
+    "revision": "565a57790e190fec06a5793e260d809f",
+    "url": "/slot-machine/static/media/cherry.565a5779.png"
+  }
+]);
